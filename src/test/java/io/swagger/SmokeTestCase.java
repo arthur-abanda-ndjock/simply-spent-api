89cbb41d946ff34ec7ff39beb7ff8c314784962f
arthur-abanda-ndjock <ndjockabanda_a@yahoo.fr>
2024-05-16 16:03:18 +0200

    setup github actions workflows for feature and main branch & jacoco

---
+++ src/test/java/io/swagger/SmokeTestCase.java
@@ -0,0 +1,16 @@
+package io.swagger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTestCase {
+
+    @Test
+    void testGetFirstCustomer() {
+        int expected = 1;
+        int actual = 1;
+        assertEquals(expected, actual);
+    }
+
+}
